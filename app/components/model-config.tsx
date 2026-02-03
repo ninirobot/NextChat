d2379424094ab3f728a81cb6f9810e837f87d4a0
ninirobot <952583052@qq.com>
2026-02-03 22:38:22 +0800

    fix: Top_P参数

--- app/components/model-config.tsx
+++ app/components/model-config.tsx
@@ -75,7 +75,7 @@ export function ModelConfigList(props: {
       >
         <InputRange
           aria={Locale.Settings.TopP.Title}
-          value={(props.modelConfig.top_p ?? 1).toFixed(1)}
+          value={(props.modelConfig.top_p ?? 1).toFixed(2)}
           min="0"
           max="1"
           step="0.05"
